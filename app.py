6865b423ced92326ae6bf65d1479ade4729888b8
Xeberlhyn <47871797+VTEAMBOT@users.noreply.github.com>
2023-12-29 04:53:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,9 +94,9 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 app.logger.setLevel(logging.INFO)
 
 
-channel_secret = os.getenv('c1a32bb792d33105b21b0e4ffeea680f')
-handler = WebhookHandler('c1a32bb792d33105b21b0e4ffeea680f')
-channel_access_token = os.getenv('9JbP+PRmPFu1AU5s2cMeUCRiD0H/WTg+1G6N0iqQtmwbyqo8t44wTKJIhfr2DOqEzfzrQ1UpI2tIG0NnV3AWbiL/o1mDV0w6vCHb2tSv8XkASczwcYa6vM46Dr1aBrOIYyCmxQEgJHfRR35g3PHBbwdB04t89/1O/w1cDnyilFU=')
+channel_secret = os.getenv('6784e25383ee39debbddd38dbcd7ab1f')
+handler = WebhookHandler('6784e25383ee39debbddd38dbcd7ab1f')
+channel_access_token = os.getenv('2Qch8cY6wIMUpoDKqxoe7vhUjCKTX07Ccn+Vn5hk8A1SqMu7TcvY0ablo0DDuEnaoASRcMTNFQu3buoi7MGmrbwRFEKJdzQPHgu/i1QhxqDuJy/MaCSK0sSKqnxEUOsrb5PFZPzw0FhCweL/vmfb0QdB04t89/1O/w1cDnyilFU=')
 configuration = Configuration(access_token=channel_access_token)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
